4f35cdea10f6157c01d61f1ea92f4a7f4e8c4894
fady-helal <fadyhelal78>
2024-01-28 00:55:56 +0200

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,3 +4,18 @@ if __name__ == "__main__":
     arguments = sys.argv[1:]
     if (len(sys.argv ) == 1):
         print("{} arguments.".format(len(sys.argv)))
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
