3d6c44429f15bc0fb8477d10a3c66ffaaabe51f3
fady-helal <fadyhelal78>
2024-02-04 22:51:09 +0200

    Executable!

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_set = set(my_list)
-    final_list = list(new_set)
-    sum = 0
-    for i in final_list:
-        sum += i
-    return sum
+    uniq_list = set(my_list)
+    num = 0
+    for i in uniq_list:
+        num += i
+    return (num)
