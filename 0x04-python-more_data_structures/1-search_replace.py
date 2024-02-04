0081359d3b118618ceaabdf0ee662e6798f1e056
fady-helal <fadyhelal78>
2024-02-04 22:03:14 +0200

    Executable!

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,10 +2,10 @@
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     if search > len(my_list):
-        return(my_list)
+        return (my_list)
     elif search > 0:
         new_list[search - 1] = replace
-        return(new_list)
+        return (new_list)
     elif search < 0:
         new_list[search] = replace
-        return(new_list)
+        return (new_list)
