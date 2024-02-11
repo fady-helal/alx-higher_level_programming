18893ca0fb4ffb983b0d3a1d3f6489d0eb3eccaf
fady-helal <fadyhelal78>
2024-02-11 18:46:57 +0200

    Executable!

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -58,6 +58,6 @@ class Rectangle:
         """
         rec = ""
         if self.__width != 0 and self.__height != 0:
-            rec += "\n".join("#" * self.__width 
+            rec += "\n".join("#" * self.__width \
                             for j in range(self.__height))
         return rec
