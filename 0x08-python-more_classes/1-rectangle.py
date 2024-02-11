932aeb047764496ff60774b22b820980d84bcdaa
fady-helal <fadyhelal78>
2024-02-11 12:38:16 +0200

    Executable!

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,13 +2,14 @@
 """Define a Rectangle class"""
 
 class Rectangle:
-    """Initialize a new Rectangle.
+    """Represent a rectangle."""
 
-    Args:
-    width (int): The width of the new rectangle.
-    height (int): The height of the new rectangle.
-    """
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+    Args:
+        width (int): The width of the new rectangle.
+        height (int): The height of the new rectangle.
+    """
         self.__width = width
         self.__height = height
 
