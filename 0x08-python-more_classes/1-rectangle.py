213657718dd224791165dd5538b06f423adf8257
fady-helal <fadyhelal78>
2024-02-11 12:35:27 +0200

    Executable!

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,6 +17,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif self.__width < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
     
     @property
     def height(self):
@@ -28,4 +29,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif(self.__height < 0):
             raise ValueError("height must be >= 0")
-        
+        self.__height = value
