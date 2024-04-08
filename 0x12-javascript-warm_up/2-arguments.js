d61a3edd076eae27d682b23b32947725f33e919a
fadyhelal <fadyhelal8@gmail.com>
2024-04-08 23:58:07 +0200

    Executable!

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-(process.argv.length === 2)?
-    console.log('No argument')
-    : (process.argv.length === 3)?
-        console.log('Argument found')
-    : console.log('Arguments found');
+if (process.argv.length === 2) {
+    console.log('No argument');
+    } else if (process.argv.length === 3) {
+    console.log('Argument found');
+    } else {
+    console.log('Arguments found');
+    }
