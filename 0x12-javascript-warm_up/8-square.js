ebcf83b89afc89c14449ec787b4f4aa848c0dc3f
fadyhelal <fadyhelal8@gmail.com>
2024-04-09 13:08:25 +0200

    Executable!

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+    console.log('Missing size');
+}
+for (let i = 0; i < Number(process.argv[2]); i++){
+    for (let i = 0; i < Number(process.argv[2]); i++){
+        process.stdout.write('X');
+    }
+    console.log();
+}
