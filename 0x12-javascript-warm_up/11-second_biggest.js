11e62f83057fe4cdb27dc944f9c499accb2aee85
fadyhelal <fadyhelal8@gmail.com>
2024-04-09 16:14:20 +0200

    Executable!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,25 +1,25 @@
 #!/usr/bin/node
 function secondbigest (arr) {
-    let max = -Infinity;
-    let result = -Infinity;
-    for (const value of arr) {
-      const nr = Number(value);
-  
-      if (nr > max) {
-        [result, max] = [max, nr]; // save previous max
-      } else if (nr < max && nr > result) {
-        result = nr; // new second biggest
-      }
+  let max = -Infinity;
+  let result = -Infinity;
+  for (const value of arr) {
+    const nr = Number(value);
+
+    if (nr > max) {
+      [result, max] = [max, nr]; // save previous max
+    } else if (nr < max && nr > result) {
+      result = nr; // new second biggest
     }
-  
-    console.log(result);
   }
+
+  console.log(result);
+}
+
 if (process.argv.length <= 3) {
   console.log(0);
-}
-else{
-const args = [];
-for (let i = 0; i < (Number(process.argv.length)); i++) {
-  args[i] = Number(process.argv[i]);
-}secondbigest(args);
+} else {
+  const args = [];
+  for (let i = 0; i < (Number(process.argv.length)); i++) {
+    args[i] = Number(process.argv[i]);
+  }secondbigest(args);
 }
