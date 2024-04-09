6b418f66e73c9dbf4a922f7535f7988a73a42344
fadyhelal <fadyhelal8@gmail.com>
2024-04-10 00:38:28 +0200

    Executable!

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor (w, h) {
+      if ((w > 0) && (h > 0)) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+
+  print () {
+      for (let i = 0; i < this.height; i++) {
+        for (let x = 0; x < this.width; x++) {
+          process.stdout.write('X');
+        }
+        console.log('');
+      }
+    }
+
+    rotate() {
+      let num;
+      num = this.height;
+      this.height = this.width;
+      this.width = num;
+    }
+
+    double() {
+      this.width = this.width * 2;
+      this.height = this.height * 2;
+    }
+  }
+  module.exports = Rectangle;
+  
