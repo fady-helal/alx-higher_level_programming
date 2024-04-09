b1acf5f59f74f454d3cd2432042320f9f69dca4d
fadyhelal <fadyhelal8@gmail.com>
2024-04-09 15:42:00 +0200

    Executable!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log('0');
+}
+const args = [];
+for (let i = 0; i < (Number(process.argv.length)); i++) {
+  args[i] = Number(process.argv[i]);
+}
+function secondbigest (arr) {
+  let max = -Infinity;
+  let result = -Infinity;
+  for (const value of arr) {
+    const nr = Number(value);
+
+    if (nr > max) {
+      [result, max] = [max, nr]; // save previous max
+    } else if (nr < max && nr > result) {
+      result = nr; // new second biggest
+    }
+  }
+
+  console.log(result);
+}
+secondbigest(args);
