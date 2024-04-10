11dba404cbbef964a0b89a47890788e04a229b53
fadyhelal <fadyhelal8@gmail.com>
2024-04-10 16:06:17 +0200

    Executable!

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@ exports.esrever = function (list) {
   let len = list.length - 1;
 
   for (let i = 0; i < list.length / 2; i++) {
-    let rmp = list[i];
+    const rmp = list[i];
     list[i] = list[len];
     list[len] = rmp;
     len--;
