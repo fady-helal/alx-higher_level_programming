01c8b64c1bdf8ddc429607ce7eb0901f35502d45
fadyhelal <fadyhelal8@gmail.com>
2024-04-26 02:36:56 +0300

    Executable!

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
-#!/usr/bin/python3
-
-# Check if a URL is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a request to the URL and display the size of the response body in bytes
-curl -s -w "%{size_download}\n" "$1"
+#!/bin/bash
+# Command to display the size of the content in a headr
+curl -sI $1 | grep Content-Length | cut -d" " -f2
