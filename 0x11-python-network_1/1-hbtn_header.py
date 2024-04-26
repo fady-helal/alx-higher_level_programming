c1ebfcee4919c74567709e27fb1715bb05787c77
fadyhelal <fadyhelal8@gmail.com>
2024-04-27 02:54:30 +0300

    Executable!

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,8 +8,4 @@ if __name__ == "__main__":
   url = sys.argv[1]
   req = urllib.request.Request(url)
   with urllib.request.urlopen(req) as response:
-    body = response.read()
-    headers = response.headers
-    request_id = headers.get('X-Request-Id')
-    if request_id:
-      print(request_id)
+      print(dict(response.headers).get("X-Request-Id"))
