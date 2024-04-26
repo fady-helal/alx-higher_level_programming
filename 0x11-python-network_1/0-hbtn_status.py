36f2ab2985bd0c45eb7c33badf51e7837a5b6177
fadyhelal <fadyhelal8@gmail.com>
2024-04-27 02:14:47 +0300

    Executable!

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""script that fetches https://alx-intranet.hbtn.io/status."""
-from urllib.request import urlopen
-from pprint import pprint
+"""fetches https://alx-intranet.hbtn.io/status."""
+import urllib.request
 
 
 if __name__ == "__main__":
-  with urlopen("https://alx-intranet.hbtn.io/status") as response:
-    body = response.read()
-  print("Body response:")
-  print("\t- type: {}".format(type(body)))
-  print("\t- content: {}".format(body))
-  print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
