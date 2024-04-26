9013ad9d647dd0e6e11a407f7d1843b23dc5e521
fadyhelal <fadyhelal8@gmail.com>
2024-04-27 02:01:18 +0300

    Executable!

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from urllib.request import urlopen
+from pprint import pprint
+with urlopen("https://alx-intranet.hbtn.io/status") as response:
+  body = response.read()
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode("utf-8")))
