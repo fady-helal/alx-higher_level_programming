7f38109c090464c8ce3350be9747043f2fdfa5b1
fadyhelal <fadyhelal8@gmail.com>
2024-05-12 00:22:47 +0300

    Executable!

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
 # Send a HEAD request to the URL passed as an argument and capture the status code
-STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}\n" "$1")
-
-# Display the status code
-echo $STATUS_CODE
-
+curl -s -o /dev/null -w "%{http_code}\n" "$1"
